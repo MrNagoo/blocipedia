0e4c4379b367e96673cb803011cf4846a8a8621d
Aaron Nagucki <aaron.nagucki@gmail.com>
2016-05-11 08:23:16 -0400

    half of CRUD done

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -4,11 +4,59 @@ class WikisController < ApplicationController
   end
 
   def show
+    @wiki = Wiki.find(params[:id])
   end
 
   def new
+    @wiki = Wiki.new
   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
   end
-end
+
+  def create
+    @wiki = Wiki.new
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+
+    if @wiki.save
+      flash[:notice] = "Wiki was saved succesfully"
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was an error saving you wiki"
+      render :new
+    end
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.update_attributes(wiki_params)
+
+    if @wiki.save
+      flash[:notice] = "Wiki was updated"
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "Error saving wiki"
+      render :new
+    end
+  end
+
+  def destroy
+    @wiki = Wiki.find(params[:id])
+
+    if @wiki.destroy
+      flash[:notice] = "#{@wiki.title} was deleted successfully."
+      redirect_to action: :index
+    else
+      flash.now[:alert] = "There was an error deleting the wiki."
+      render :show
+    end
+  end
+
+  private
+
+  def wiki_params
+    params.require(:wiki).permit(:title, :body, :private)
+  end
+  end
--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -16,19 +16,55 @@ RSpec.describe WikisController, type: :controller do
     end
   end
 
-  # describe "GET #show" do
-  #   it "returns http success" do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-  #
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET #show" do
+    it "returns http success" do
+      get :show, {id: my_wiki.id}
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #show view" do
+      get :show, {id: my_wiki.id}
+      expect(response).to render_template :show
+    end
+
+    it "assigns my_wiki to @wiki" do
+      get :show, {id: my_wiki.id}
+      expect(assigns(:wiki)).to eq(my_wiki)
+    end
+  end
+
+  describe "GET new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    it "instantiates @wiki" do
+      get :new
+      expect(assigns(:wiki)).not_to be_nil
+    end
+  end
+
+  describe "POST create" do
+    it "increases the number of Wiki by 1" do
+      expect{ post :create, wiki: {title: "Hello", body: "This is the body"}}.to change(Wiki,:count).by(1)
+    end
+
+    it "assigns the new wiki it @wiki" do
+      post :create, wiki: {title: "Hello", body: "This is the body"}
+      expect(assigns(:wiki)).to eq Wiki.last
+    end
+
+    it "redirects to the new wiki" do
+      post :create, wiki: {title: "Hello", body: "This is the body"}
+      expect(response).to redirect_to Wiki.last
+    end
+  end
   #
   # describe "GET #edit" do
   #   it "returns http success" do
